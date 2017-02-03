bd544b743c22a43ce6d1720d18d141be768d4617
Stephen Corry <stevemcorry@gmail.com>
2017-02-03 09:39:30 -0700

    add comments

--- src/modals/modal-basic/modal-basic.ts
+++ src/modals/modal-basic/modal-basic.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-
+ //Import the view controller (used to close the modal)
 import { ViewController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'modal-basic.html'
 })
 export class ModalBasic {
+//inject the view controller
   constructor(public viewCtrl: ViewController) { }
 
   dismiss(){
+  // close the modal
       this.viewCtrl.dismiss();
   }
 }
