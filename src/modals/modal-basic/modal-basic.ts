206dd441c49f38d6fcdcf33fca89abc7fc67fe38
Stephen Corry <stevemcorry@gmail.com>
2017-02-03 09:39:09 -0700

    add comments

--- src/modals/modal-basic/modal-basic.ts
+++ src/modals/modal-basic/modal-basic.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
-import { ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'modal-basic.html'
 })
 export class ModalBasic {
-  constructor(public modalCtrl: ModalController, public viewCtrl: ViewController) { }
+  constructor(public viewCtrl: ViewController) { }
 
   dismiss(){
       this.viewCtrl.dismiss();
--- src/modals/modalParams/modalParams.ts
+++ src/modals/modalParams/modalParams.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
-import { ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
+import { NavParams, ViewController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'modalParams.html'
 })
 export class ModalParams {
-  constructor(public modalCtrl: ModalController, public viewCtrl: ViewController, public params: NavParams) {
+  constructor(public viewCtrl: ViewController, public params: NavParams) {
     this.param = params.get('param');
    }
 
